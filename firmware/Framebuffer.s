efd102c7616cf8b2f818fa4feb33195cdcc2716e
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-03-04 16:38:59 -0600

    Some FORTH stuff
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- firmware/Framebuffer.s
+++ firmware/Framebuffer.s
@@ -1,17 +1,5 @@
-.text
-.global FramebufferClear: /* (color: a0) */
-    la t0, 0x90001000
-    la t1, 0x900c1000
-.loop:
-    sb a0, 0(t0)
-    addi t0, t0, 1
-    bltu t0, t1, .loop
-    blr zero, ra
-
 .rodata
 OkamiLogo: .include_bin "Images/OkamiLogo.dat"
-
-SCSIDriveIcon: .include_bin "Images/SCSIDrive.dat"
 
 Unifont: .include_bin "Images/Unifont.dat"
 .align 4
