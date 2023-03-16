7797cbd355c3ef43ba39f2618683b58a6a7d6c7b
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-03-15 22:00:33 -0500

    Yet another commit full of SCSI related things
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- firmware/SCSI.s
+++ firmware/SCSI.s
@@ -33,6 +33,33 @@ SCSISelect: /* (a0: Initiator ID a1: Target ID, a2: Reselect) */
     ori t1, t1, 1
     sw t1, 12(t0)
 .after_reselect1:
-    or 
+    ori t1, a0, a1
+    sw t1, 0(t0)
+    lw t1, 4(t0)
+    ori t1, t1, 0x29
+    sw t1, 4(t0)
+    lw t1, 8(t0)
+    andi t1, t1, 0xfe
+    sw t1, 8(t0)
+    sw zero, 16(t0)
+    lw t1, 4(t0)
+    andi t1, t1, 0xf7
+    sw t1, 4(t0)
+    lui t2, 0x10
+.assertloop:
+    lw t1, 4(t0)
+    andi t1, t1, 4
+    beq t1, zero, .ret
+    addi t2, t2, -1
+    bltu zero, t2, .assertloop /* bgtu t2, zero, .assertloop */
+    li a0, 1
+    br ra
+.ret:
+    lw t1, 4(t0)
+    andi t1, t1, 0xfa
+    beq a2, zero, .after_reselect2
+    ori t1, t1, 0x7
+.after_reselect2:
+    sw t1, 4(t0)
     li a0, 0
     br ra
