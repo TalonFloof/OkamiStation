25b61920dca40301de2378833d8998d1678f5c70
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-03-27 20:40:47 -0500

    I promise I'm trying!
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- Fennec/kernel/FennecKernel.pas
+++
@@ -1,5 +0,0 @@
-program FennecKernel;
-
-begin
-
-end.
---
+++ okamisdk/okcc/notice.h
@@ -0,0 +1,6 @@
+/*
+** NOTICE.H -- Small C Signon Notice.
+*/
+#define VERSION "Okami1041 C Compiler\nRelease 0.1\n"
+#define CRIGHT1 "Copyright 2023 TalonFox\nBased on Simple C, Copyright 2003 Charles Childers"
+#define CRIGHT2	"Based on Small C, Copyright 1982, 1983, 1985, 1988 J. E. Hendrix\nCopyright 1998  H T Walheim\n\n"
