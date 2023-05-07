95a7d65ffc191fb98f0a00050ee007cb2b9d6bf7
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-05-07 08:34:04 -0500

    eeeeghfop[
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- Fennec/bootloader/okami1041/Bootstrap.s
+++ Fennec/bootloader/okami1041/Bootstrap.s
@@ -13,13 +13,30 @@
     li a1, 1
     mv t7, a2
     mcall 0x201
-    lw a0, 4(t7)
+    lw a0, 4(t7) /* Read the first Inode Block */
+    addi a0, a0, 64
     li a1, 1
     mv t6, a2
     mcall 0x201
-
+    mv t6, t5
+    addi t5, t5, 256
+    lw t0, 48(t5) /* Get the first zone */
 .halt:
     beq zero, zero, .halt
+
+.global ReadZone: /* (a0: Zone, a2: Address) | (a0: Next Zone) */
+    addi sp, sp, -4
+    sw a0, 4(sp)
+    lw t0, 24(t7)
+
+    lw t0, 16(t7)
+    addi t0, t0, 64
+    
+
+    addi sp, sp, 4
+    lw a0, 0(sp)
+    br ra
+
 .rodata
 banner: .ascii "Fennec Bootloader (for OkamiStation 1000)" .byte 0xa .ascii "(C) 2023 TalonFox, Licensed under the MIT License" .byte 0xa .byte 0xa .byte 0
 .bss
